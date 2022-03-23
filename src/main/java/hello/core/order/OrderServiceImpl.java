cb8db02416bf0daa5f0f948a180e0b7aa7af25c0
henn36 <henn36@naver.com>
2022-03-23 23:02:36 +0900

    New Discount Policy(RateDiscountPolicy)

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,7 +1,7 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +9,13 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    /**
+     * 할인 정책의 클라이언트인 OrderServiceImpl은 DiscountPolicy 인터페이스 뿐만 아니라,
+     * 구현체인 Fix/RateDiscountPolicy도 함께 의존중 (OCP 위반!)
+     *  **/
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // 할인 정책 변경
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
